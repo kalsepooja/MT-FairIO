5dfaaf0ddab43cc9aadfadb7f15a8a357f2a1904
kalse pooja <kalsepooja056@gmail.com>
2024-04-21 12:28:59 +0530

    machine test on fair task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FairDashboardComponent } from './shared/component/fair-dashboard/fair-dashboard.component';
+import { FairDetailsComponent } from './shared/component/fair-details/fair-details.component';
+import { FairCardComponent } from './shared/component/fair-card/fair-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FairDashboardComponent,
+    FairDetailsComponent,
+    FairCardComponent
   ],
   imports: [
     BrowserModule
